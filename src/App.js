7f4c3d48a341d1a2c37a91535bdb06f07da0f6b2
abeez07 <abrahamyusuf17@gmail.com>
2023-11-10 18:39:19 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+   <div class=" d-flex  mx-auto mt-5" >
+  
+  <div class="card-body">
+    <h2 id='h2' class="card-title fs-4 mb-4  text-center text-white fw-bolder ">Be among the first to enjoy <br /> seamless shopping and loads of <br /> benefits when we LAUNCH!</h2>
+    <h2 id='h2' class="card-title fs-4  text-center text-white">JOIN THE WAITLIST!</h2>
+
+   <div className="col-md-5 mx-auto">
+   <label id='h2' className='text-white' htmlFor="">Full Name</label>
+    <input  className='form-control  mb-4'  type="text" name="Name" placeholder='Full Name' id="" />
+    
+   </div>
+   <div className="col-md-5 mx-auto">
+    <label id='h2' className='text-white' htmlFor="">Email Address</label>
+    <input className='form-control mb-4' type="email" name="" placeholder='Email address' id="" />
     </div>
-  );
+    <div className="col-md-5 mx-auto">
+    <label id='h2' className='text-white' htmlFor="">Phone Number</label>
+    <input className='form-control mb-4'  type="text" name="Name" placeholder='Phone Number' id="" />
+    </div>
+    <div className="col-md-5 mx-auto">
+    <label id='h2' className='text-white' htmlFor="">Country</label>
+    <input className='form-control mb-4' type="text" name="" placeholder='Country' id="" />
+    </div>
+    <div class="d-flex justify-content-center col-md-5 mx-auto">
+    <button id='h2' class="btn btn-outline-dark">Join WaitList ↦ </button>
+    </div>
+  </div>
+</div>
+
+   </>
+  )
 }
 
-export default App;
+export default App
